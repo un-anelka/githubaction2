b7810de64dd845e9dabe84ed7cf2f70ebddd40a0
UN Francois RUDASINGWA <un.rudasingwa@gmail.com>
2022-06-20 01:14:32 +0200

    #This is a combination of 17 commits.
    Deployment to heroku with github actions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("UN actions rrrr")
+const express = require("express"),
+    app = express();
+
+const PORT = process.env.PORT || 3007
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.send({ ok: true })
+}) 
